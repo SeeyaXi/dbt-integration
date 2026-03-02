bf007d68e9d47ff3def985267dd0415e932847b2
SeeyaXi <xigua950113@outlook.com>
2026-03-02 15:39:59 +0800

    Update init_all.sql

--- scripts/init_all.sql
+++ scripts/init_all.sql
@@ -1,2 +1,2 @@
 EXECUTE IMMEDIATE FROM 'init/dummy_script.sql';
-CALL XIYJ_DB.PUBLIC.RUN_SCRIPT_ONCE('/scripts/init/dummy_script.sql');
+CALL XIYJ_DB.DBT.RUN_SCRIPT_ONCE('/scripts/init/dummy_script.sql');
